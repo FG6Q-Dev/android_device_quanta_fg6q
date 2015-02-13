c4de6bfbfd760858bcbe5b4ef2f230aa840b1f69
Spartaner25 <spartaner25@hotmail.de>
2015-02-13 16:35:26 +0100

    Added Initscripts

--- cm.mk
+++ cm.mk
@@ -1,11 +1,8 @@
-## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
 # Release name
 PRODUCT_RELEASE_NAME := fg6q
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/quanta/fg6q/device_fg6q.mk)
--- device_fg6q.mk
+++ device_fg6q.mk
@@ -7,6 +7,16 @@ $(call inherit-product-if-exists, vendor/quanta/fg6q/fg6q-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/quanta/fg6q/overlay
 
+# Root
+PRODUCT_COPY_FILES += \
+	device/quanta/fg6q/fstab.macallan:root/fstab.macallan \
+	device/quanta/fg6q/ueventd.macallan.rc:root/ueventd.macallan.rc
+
+# Init
+PRODUCT_COPY_FILES += \
+	device/quanta/fg6q/init/init.macallan.rc:root/init.macallan.rc \
+	device/quanta/fg6q/init/init.macallan.usb.rc:root/init.macallan.usb.rc \
+
 LOCAL_PATH := device/quanta/fg6q
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
