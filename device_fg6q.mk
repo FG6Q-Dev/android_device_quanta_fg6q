3f36366ce37491c129c435a88beee9628c46e44d
Spartaner25 <spartaner25@hotmail.de>
2015-02-15 18:07:30 +0100

    Fixed Init, ADB over USB is working now

--- device_fg6q.mk
+++ device_fg6q.mk
@@ -15,6 +15,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
 	device/quanta/fg6q/init/init.macallan.rc:root/init.macallan.rc \
 	device/quanta/fg6q/init/init.macallan.usb.rc:root/init.macallan.usb.rc \
+	device/quanta/fg6q/init/init.tf.rc:root/init.tf.rc
 
 # Media
 PRODUCT_COPY_FILES += \
