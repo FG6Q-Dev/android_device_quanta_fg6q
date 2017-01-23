84f961d47097bf4846c100e889115dd78d8eb4a0
Spartaner25 <spartaner25@hotmail.de>
2017-01-23 22:51:52 +0100

    Merge makefiles

--- sensors/Android.mk
+++ sensors/Android.mk
@@ -18,6 +18,34 @@ LOCAL_PATH:= $(call my-dir)
 
 ifneq ($(TARGET_SIMULATOR),true)
 
+# HAL module implemenation, not prelinked and stored in
+# hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.board.platform>.so
+include $(NVIDIA_DEFAULTS)
+
+LOCAL_CFLAGS := -DLOG_TAG=\"Sensors\"
+LOCAL_SRC_FILES := sensors.cpp
+
+LOCAL_C_INCLUDES += $(LOCAL_PATH)
+LOCAL_C_INCLUDES += device/nvidia/drivers/sensors
+LOCAL_C_INCLUDES += device/nvidia/drivers/sensors/mlsdk/driver/include
+LOCAL_C_INCLUDES += device/nvidia/drivers/sensors/mlsdk/driver/include/linux
+LOCAL_C_INCLUDES += device/nvidia/drivers/sensors/mlsdk/HAL/include
+LOCAL_C_INCLUDES += device/nvidia/drivers/sensors/mlsdk/mllite
+LOCAL_C_INCLUDES += device/nvidia/drivers/sensors/mlsdk/mllite/linux
+LOCAL_C_INCLUDES += device/nvidia/drivers/sensors/mlsdk/mpl
+LOCAL_SHARED_LIBRARIES := liblog libcutils libutils libdl libsensors.base \
+                          libinvensense_hal libsensors.mpl \
+                          libsensors.cm3217 libsensors.nvs_input
+LOCAL_CPPFLAGS+=-DLINUX=1
+LOCAL_PRELINK_MODULE := false
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_MODULE := sensors.tegratab
+
+include $(NVIDIA_SHARED_LIBRARY)
+
 include $(NVIDIA_DEFAULTS)
 LOCAL_MODULE := libsensors.base
 LOCAL_MODULE_TAGS := optional
