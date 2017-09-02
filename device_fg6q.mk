a3ae2cc3e01da2b0652eef074ddc85eb483b29cd
Spartaner25 <spartaner25@hotmail.de>
2017-09-02 21:36:36 +0200

    Switch to oss implementation

--- device_fg6q.mk
+++ device_fg6q.mk
@@ -123,7 +123,8 @@ PRODUCT_PACKAGES += \
 # OSS HW Modules
 PRODUCT_PACKAGES += \
     lights.macallan \
-    power.macallan
+    power.macallan \
+    sensors.macallan
 
 # libshims
 PRODUCT_PACKAGES += \
@@ -137,10 +138,6 @@ PRODUCT_COPY_FILES += \
     device/quanta/fg6q/prebuilts/libnvmm_camera.so:system/vendor/lib/libnvmm_camera.so \
     device/quanta/fg6q/prebuilts/libnvodm_imager.so:system/vendor/lib/libnvodm_imager.so \
     device/quanta/fg6q/prebuilts/libnvodm_query.so:system/vendor/lib/libnvodm_query.so \
-    device/quanta/fg6q/prebuilts/sensors.macallan.so:system/lib/hw/sensors.macallan.so \
-    device/quanta/fg6q/prebuilts/libinvensense_hal.so:system/lib/libinvensense_hal.so \
-    device/quanta/fg6q/prebuilts/libmplmpu.so:system/lib/libmplmpu.so \
-    device/quanta/fg6q/prebuilts/libmllite.so:system/lib/libmllite.so \
     device/quanta/fg6q/prebuilts/gps.tegra.so:system/lib/hw/gps.tegra.so \
     device/quanta/fg6q/prebuilts/glgps_nvidiaTegra2android:system/bin/glgps_nvidiaTegra2android
 
